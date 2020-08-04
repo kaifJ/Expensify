7a5f46c7f631d3c9ce6c84def9829a03e18674cc
Kaif Jamadar <kaifjamadar@Kaifs-MacBook.local>
2020-08-04 19:36:29 +0530

    Basic Client Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <Fragment>
+      <Navbar />
       <h1>Rect App</h1>
     </Fragment>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react'
+
+const Navbar = () => (
+    <Fragment>
+        <h2>Navbar</h2>
+    </Fragment>
+)
+
+export default Navbar
