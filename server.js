bca0ce608a10d1649019063bec6193180c4bc95e
Kaif Jamadar <kaifjamadar@Kaifs-MacBook.local>
2020-08-13 21:29:44 +0530

    Final touches

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use('/api/expense', require('./src/routes/api/expense'))
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
-    app.get('*', res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 const PORT = process.env.PORT || 6000
